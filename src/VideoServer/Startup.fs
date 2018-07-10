76aaeddb4905e77ab25fe56b5645d45f018886ff
wk-j <somnuk.wk@gmail.com>
2018-07-11 02:30:02 +0700

    Initial commit

---
+++ src/VideoServer/Startup.fs
@@ -0,0 +1,32 @@
+namespace VideoServer
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.HttpsPolicy;
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+
+type Startup private () =
+    new (configuration: IConfiguration) as this =
+        Startup() then
+        this.Configuration <- configuration
+
+    member __.ConfigureServices(services: IServiceCollection) =
+        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1) |> ignore
+
+    member __.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
+        if (env.IsDevelopment()) then
+            app.UseDeveloperExceptionPage() |> ignore
+        else
+            app.UseHsts() |> ignore
+
+        app
+            .UseStaticFiles()
+            .UseMvc() |> ignore
+
+    member val Configuration : IConfiguration = null with get, set
